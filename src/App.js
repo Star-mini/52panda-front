b52a9b649401d7e4ab37613ac41d35644f1a63ae
taewoo <dlscksdlsi2@gmail.com>
2024-04-08 15:20:14 +0900

    feat:프로젝트 기본구성

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Auction from "../src/components/pages/Auction";
+import ItemDetail from "../src/components/pages/ItemDetail";
+import SignUp from "../src/components/pages/SignUp";
+import MyPage from "../src/components/pages/MyPage";
+import index from "../src/components/pages/index";
+import Login from "../src/components/pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<index />} />
+      <Route path="/auction" element={<Auction />} />
+      <Route path="/detail" element={<ItemDetail />} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="/mypage" element={<Login />} />
+      <Route path="/mypage" element={<SignUp />} />
+    </Routes>
   );
 }
 
