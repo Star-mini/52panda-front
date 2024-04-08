1efe2a2bda40e7fe52668413b6b5bcd58c339967
taewoo <dlscksdlsi2@gmail.com>
2024-04-08 17:35:55 +0900

    feat: navbar 제작중

--- src/components/commons/navbar/Navbar.js
+++ src/components/commons/navbar/Navbar.js
@@ -1,6 +1,44 @@
 import React from "react";
+import pandaLogo from "../../../static/styles/images/panda_logo.png";
+import heartButton from "../../../static/styles/images/heart_black.png";
+import styles from "../../../static/styles/css/mainpage.module.css";
 function Navbar() {
-  return;
+  return (
+    <nav class="navbar navbar-expand bg-body-tertiary">
+      <div className="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src={pandaLogo} width="50" height="24" alt="" />
+        </a>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="" href="">
+                <p className={styles.p}>About</p>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="">
+                <p className={styles.p}>Item</p>
+              </a>
+            </li>
+            <li className="nav-item">
+              <input />
+            </li>
+            <li className="nav-item">
+              <button className={styles.button}>
+                <img
+                  className={styles.img}
+                  width="20px"
+                  src={heartButton}
+                  alt=""
+                />
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
