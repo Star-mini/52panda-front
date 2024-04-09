ef6cdfd250c3ed9bb97f0bad751d84106f001764
taewoo <dlscksdlsi2@gmail.com>
2024-04-09 10:49:01 +0900

    feat: navbar 60%완성

--- src/components/commons/navbar/Navbar.js
+++ src/components/commons/navbar/Navbar.js
@@ -1,35 +1,57 @@
 import React from "react";
 import pandaLogo from "../../../static/styles/images/panda_logo.png";
 import heartButton from "../../../static/styles/images/heart_black.png";
-import styles from "../../../static/styles/css/mainpage.module.css";
+import styles from "../../../static/styles/css/navbar.module.css";
+import mypageButton from "../../../static/styles/images/mypage_panda.png";
+
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand bg-body-tertiary">
+    <nav class="navbar navbar-expand">
       <div className="container-fluid">
         <a class="navbar-brand" href="#">
-          <img src={pandaLogo} width="50" height="24" alt="" />
+          <img src={pandaLogo} width="120" height="55" alt="" />
         </a>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="" href="">
+            <li className="nav-item ms-4">
+              <a className={styles.a} href="">
                 <p className={styles.paragraph}>About</p>
               </a>
             </li>
-            <li className="nav-item">
-              <a href="">
+            <li className="nav-item ms-2">
+              <a className={styles.a} href="">
                 <p className={styles.paragraph}>Item</p>
               </a>
             </li>
+            <li className="nav-item ms-2">
+              <a className={styles.a} href="">
+                <p className={styles.paragraph}>Post</p>
+              </a>
+            </li>
+          </ul>
+          <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <input className={styles.input} />
+              <input
+                className={`${styles.input} form-control`}
+                placeholder="찾고 싶은 중고물품을 검색해보세요"
+              />
             </li>
             <li className="nav-item">
               <button className={styles.button}>
                 <img
                   className={styles.img}
-                  width="20px"
+                  width="40px"
                   src={heartButton}
+                  alt=""
+                />
+              </button>
+            </li>
+            <li className="nav-item">
+              <button className={styles.button}>
+                <img
+                  className={styles.img}
+                  width="40px"
+                  src={mypageButton}
                   alt=""
                 />
               </button>
