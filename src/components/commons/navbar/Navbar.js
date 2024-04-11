c77410c8f3cf203cb25bdcd89b69bce3de4bfe9e
taewoo <dlscksdlsi2@gmail.com>
2024-04-11 14:41:12 +0900

    feat: navbar완성,반응형완벽하진않음

--- src/components/commons/navbar/Navbar.js
+++ src/components/commons/navbar/Navbar.js
@@ -3,27 +3,34 @@ import pandaLogo from "../../../static/styles/images/panda_logo.png";
 import heartButton from "../../../static/styles/images/heart_black.png";
 import styles from "../../../static/styles/css/navbar.module.css";
 import mypageButton from "../../../static/styles/images/mypage_panda.png";
+import dbg from "../../../static/styles/images/dbg.svg";
 
 function Navbar() {
   return (
     <nav class="navbar navbar-expand">
       <div className="container-fluid">
-        <a class="navbar-brand" href="#">
-          <img src={pandaLogo} width="120" height="55" alt="" />
+        <a className="navbar-brand" href="#">
+          <img src={pandaLogo} className={styles.logoImg} alt="" />
         </a>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item ms-4">
+        <div className="collapse navbar-collapse" id="">
+          <ul className={`navbar-nav `}>
+            <li
+              className={`nav-item ms-4 ${styles.zeromargin} ${styles.leftandblock}`}
+            >
               <a className={styles.a} href="">
                 <p className={styles.paragraph}>About</p>
               </a>
             </li>
-            <li className="nav-item ms-2">
+            <li
+              className={`nav-item ms-4 ${styles.zeromargin} ${styles.leftandblock}`}
+            >
               <a className={styles.a} href="">
                 <p className={styles.paragraph}>Item</p>
               </a>
             </li>
-            <li className="nav-item ms-2">
+            <li
+              className={`nav-item ms-4 ${styles.zeromargin} ${styles.leftandblock}`}
+            >
               <a className={styles.a} href="">
                 <p className={styles.paragraph}>Post</p>
               </a>
@@ -31,10 +38,15 @@ function Navbar() {
           </ul>
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <input
-                className={`${styles.input} form-control`}
-                placeholder="찾고 싶은 중고물품을 검색해보세요"
-              />
+              <form className="input-group">
+                <input
+                  className={`${styles.input} form-control`}
+                  placeholder="찾고 싶은 중고물품을 검색해보세요"
+                />
+                <button className={styles.button}>
+                  <img className={styles.dbg} src={dbg}></img>
+                </button>
+              </form>
             </li>
             <li className="nav-item">
               <button className={styles.button}>
@@ -46,15 +58,46 @@ function Navbar() {
                 />
               </button>
             </li>
-            <li className="nav-item">
-              <button className={styles.button}>
-                <img
-                  className={styles.img}
-                  width="40px"
-                  src={mypageButton}
-                  alt=""
-                />
-              </button>
+            <li class="dropdown nav-item me-5">
+              <a
+                class="dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <button className={styles.button}>
+                  <img
+                    className={styles.img}
+                    width="40px"
+                    src={mypageButton}
+                    alt=""
+                  />
+                </button>
+              </a>
+
+              <ul class={`${styles.dropdown} dropdown-menu`}>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    찜한목록
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    경매글목록
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    참여한 경매글목록
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    비밀번호수정
+                  </a>
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
--- src/components/pages/MyPage.js
+++ src/components/pages/MyPage.js
@@ -1,7 +1,13 @@
 import React from "react";
 import Navbar from "../commons/navbar/Navbar";
+import head from "../../static/styles/images/head.png";
 function MyPage() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />;
+      <img src={head} alt="" />
+    </div>
+  );
 }
 
 export default MyPage;
