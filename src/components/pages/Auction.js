ac5d878dcf8d24f6be75414b3322096419124d91
kjh <akkaa1119@naver.com>
2024-04-13 20:42:16 +0900

    feat: 물품을 표시한 ItemListInfoCard 추가

--- src/components/pages/Auction.js
+++ src/components/pages/Auction.js
@@ -1,7 +1,30 @@
 import React from "react";
-import Navbar from "../commons/navbar/Navbar";
+import ItemListInfoCard from "../commons/card/ItemListInfoCard";
+import noImage from "../../static/styles/images/noimage.png";
+
 function Auction() {
-  return <Navbar />;
+  return (
+    <div>
+      <div className="item-list">
+        <ItemListInfoCard
+          image={noImage}
+          title="푸바오"
+          category="IT"
+          tradingMethod= {2}
+          startPrice={13000}
+          currentPrice={230000}
+        />
+        <ItemListInfoCard
+          image={noImage}
+          title="아이바오"
+          category="의류"
+          tradingMethod= {0}
+          startPrice={1000}
+          currentPrice={20000}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Auction;
