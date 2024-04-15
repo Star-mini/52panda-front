86f0e4e878d4886cea5b8f08cd1e2e8d951a5542
Sang Min Park <minisstar@naver.com>
2024-04-15 17:57:28 +0900

    💄 경매 주문창 UI개선

--- src/components/AmountSelection.js
+++ src/components/AmountSelection.js
@@ -10,13 +10,17 @@ function AmountSelection() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <span className={styles.amount}>{amount.toLocaleString()}원</span>
-        <button className={styles.withdraw}>출금 인출</button>
-      </div>
-      <div className={styles.question}>
-        <span>얼마에 출금요?</span>
-      </div>
+<div className={styles.header}>
+  <span className={styles.amount}>{amount.toLocaleString()}원</span>
+  <button className={styles.withdraw}>출금 인출</button>
+</div>
+<div className={styles.question}>
+  <span>얼마에 출금요?</span>
+  <button className={styles.bidButton}>입찰</button> {/* 입찰 버튼 추가 */}
+</div>
+<div className={styles.maxAmount}> {/* 최대 가능 금액 표시를 위한 새로운 div */}
+  <span>최대 5000만원 가능</span>
+</div>
       <div className={styles.buttons}>
         <button className={styles.button} onClick={() => handleAddAmount(1000)}>+1000</button>
         <button className={styles.button} onClick={() => handleAddAmount(2000)}>+2000</button>
