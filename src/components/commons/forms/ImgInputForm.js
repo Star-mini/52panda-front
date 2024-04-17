abff8213d54e34d2d6937f2d9f932acce3575332
hahyeon-cho <mondlicht3724@gmail.com>
2024-04-18 00:38:24 +0900

    fix: 이미지 삭제 후 재등록에 관한 에러 해결

--- src/components/commons/forms/ImgInputForm.js
+++ src/components/commons/forms/ImgInputForm.js
@@ -1,41 +1,54 @@
 import React, { useState } from 'react';
-import { Image, Button } from 'react-bootstrap';
+import { Form, Image, Button } from 'react-bootstrap';
 import styles from '../../../static/styles/css/imgInputForm.module.css';
 
 const ImgInputForm = () => {
-  const [itemImg, setItemImg] = useState([]);
+  const [itemImgs, setItemImgs] = useState([]);
 
-  const onImgChange = (event) => {
-    event.preventDefault();
-    if (event.target.files) {
-      const imgFile = event.target.files[0];
-      const img = window.URL.createObjectURL(imgFile);
+  const onImgChange = (e) => {
+    e.preventDefault();
+    const files = e.target.files;
 
-      setItemImg((prev) => {
-        if (!prev[0]) return [{ id: 1, img }];
-        const nextId = prev[prev.length - 1].id + 1;
-        return [...prev, { id: nextId, img }];
+    if (files && files.length) {
+      const newImgs = Array.from(files).map((file, idx) => {
+        const img = window.URL.createObjectURL(file);
+        return {
+          id: itemImgs.length + idx + 1,
+          img
+        };
       });
+
+      setItemImgs((prev) => [...prev, ...newImgs]);
+      e.target.value = null;
     }
   };
 
-  const onImgDelete = (clickedId) => {
-    setItemImg((prev) => prev.filter((item) => item.id !== clickedId));
+  const onImgDelete = (deleteImg) => {
+    const imgToRevoke = itemImgs.find(item => item.id === deleteImg).img;
+    window.URL.revokeObjectURL(imgToRevoke);
+
+    setItemImgs((prev) => prev.filter((item) => item.id !== deleteImg));
   };
 
   return (
     <>
-      <input type="file" accept="image/*" onChange={onImgChange} />
-      {itemImg.map((item) => (
-        <div key={item.id}>
-          <Image src={item.img} className={styles.uploadedImg} thumbnail />
-          <Button onClick={() => onImgDelete(item.id)}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-              <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
-            </svg>
-          </Button>
-        </div>
-      ))}
+      <Form.Group controlId="img-upload-form" className="mb-3">
+        <Form.Control
+          type="file" accept="image/*" multiple
+          className='mb-3'
+          onChange={onImgChange} />
+        <div>{itemImgs.length} / 10</div>
+        {itemImgs.map((item) => (
+          <div key={item.id} className={styles['uploaded-img-container']}>
+            <Image src={item.img} className={styles['uploaded-img']} thumbnail />
+            <Button className={styles['delete-img-btn']} onClick={() => onImgDelete(item.id)}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+              </svg>
+            </Button>
+          </div>
+        ))}
+      </Form.Group>
     </>
   );
 };
