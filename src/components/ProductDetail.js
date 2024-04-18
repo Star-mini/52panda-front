452b95c52f67c50ffd34f92b110f7992c39387ca
Sang Min Park <minisstar@naver.com>
2024-04-18 16:08:01 +0900

    :style 클릭시 분홍색 하트로 변경

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -3,13 +3,15 @@ import styles from '../static/styles/css/ProductDetail.module.css';
 import placeholder from '../static/styles/images/placeholder.png';
 import PriceList from '../components/PriceList'; 
 import AmountSelection from '../components/AmountSelection';
-import heartIcon from '../static/styles/images/heart.png'; // 하트 이미지를 import 합니다.
+import heartIcon from '../static/styles/images/heart.png';
 import closeIcon from '../static/styles/images/close.png';
 import iphone from '../static/styles/images/iphone.png';
+import PinkHeart from '../static/styles/images/PinkHeart.png';
 
 function ProductDetail() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [isPopupVisible, setIsPopupVisible] = useState(false);
+  const [isHeartPink, setIsHeartPink] = useState(false);
 
   const images = [
     iphone,
@@ -24,6 +26,10 @@ function ProductDetail() {
     setCurrentImageIndex(newIndex);
   };
 
+  const toggleHeart = () => {
+    setIsHeartPink(!isHeartPink); // 하트 이미지 상태를 반전시킵니다.
+  };
+  
   const togglePopup = () => {
     setIsPopupVisible(!isPopupVisible);
   };
@@ -44,7 +50,7 @@ function ProductDetail() {
 
       <div className={styles['productDetails']}>
         <h4 className={styles['productTitle']}>아이폰 13프로</h4>
-        <img src={heartIcon} className={styles.heart} alt="Heart" /> {/* 하트 이미지를 삽입합니다. */}
+        <img src={isHeartPink ? PinkHeart : heartIcon} className={styles.heart} alt="Heart" onClick={toggleHeart} /> {/* 하트 이미지 클릭 이벤트 추가 */}
         <p className={styles.category}>전자제품/휴대폰/아이폰</p>
         <p className={styles['timeRemaining']}>낙찰까지 <span id={styles.bidTime}>13:03:93</span></p>
         <div className={styles['biddingDetails']}>
