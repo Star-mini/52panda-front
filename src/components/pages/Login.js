8dcb87d48d2edd0b5640f6e764e3bc0ecfaa0903
taewoo <dlscksdlsi2@gmail.com>
2024-04-19 16:09:32 +0900

    feat: 메인페이지완성

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,15 +1,15 @@
 import React from "react";
-import '../../static/styles/css/login.css';
+import "../../static/styles/css/login.css";
 import SocialLogin from "../commons/SocialLogin";
 
 function Login() {
-  return(
-  <div className="App">
-    <div className="box">
-      <SocialLogin/>
+  return (
+    <div className="App">
+      <div className="box">
+        <SocialLogin />
+      </div>
     </div>
-</div>
-)
+  );
 }
 
 export default Login;
--- src/components/pages/MyPage.js
+++ src/components/pages/MyPage.js
@@ -2,12 +2,7 @@ import React from "react";
 import Navbar from "../commons/navbar/Navbar";
 import head from "../../static/styles/images/head.png";
 function MyPage() {
-  return (
-    <div>
-      <Navbar />;
-      <img src={head} alt="" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default MyPage;
