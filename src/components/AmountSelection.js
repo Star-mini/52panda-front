60f1f6db901dfc15550d357c53b3692e55e8aeee
Sang Min Park <minisstar@naver.com>
2024-04-20 12:58:55 +0900

    feat:입찰버튼 기능 구현 완료

--- src/components/AmountSelection.js
+++ src/components/AmountSelection.js
@@ -7,11 +7,16 @@ function AmountSelection({ onBid }) {
 
   const handleAddAmount = (addValue) => {
     setAmount((prevAmount) => prevAmount + addValue);
+    setBidValue((prevBidValue) => {
+      // 기존 bidValue를 숫자로 변환하고, 새로운 addValue를 더한 후 다시 문자열로 변환
+      const newBidValue = parseInt(prevBidValue || '0') + addValue;
+      return newBidValue.toString();
+    });
   };
 
   const handleBid = () => {
     if (bidValue) { // 입찰가가 입력된 경우
-      onBid("사용자", bidValue); // 사용자 이름과 입찰가 전달
+      onBid("사용자", bidValue); // 사용자 이름과 입찰가 전달 (문자열로 유지)
       setBidValue(""); // 입력 필드를 비웁니다.
     }
   };
@@ -20,20 +25,20 @@ function AmountSelection({ onBid }) {
     <div className={styles.container}>
       <div className={styles.header}>
         <span className={styles.amount}>{amount.toLocaleString()}원</span>
-        <button className={styles.withdraw}>출금 인출</button>
+        <button className={styles.withdraw}>즉시 입찰</button>
       </div>
       <div className={styles.question}>
-      <input
-  type="text"
-  className={styles.inputArea}
-  placeholder="얼마에 살까요?"
-  value={bidValue}
-  onChange={(e) => {
-    const newValue = e.target.value;
-    const filteredValue = newValue.replace(/[^0-9]/g, ""); // 숫자 이외의 문자 제거
-    setBidValue(filteredValue);
-  }}
-/>
+        <input
+          type="text"
+          className={styles.inputArea}
+          placeholder="얼마에 살까요?"
+          value={bidValue}
+          onChange={(e) => {
+            const newValue = e.target.value;
+            const filteredValue = newValue.replace(/[^0-9]/g, ""); // 숫자 이외의 문자 제거
+            setBidValue(filteredValue);
+          }}
+        />
         <button className={styles.bidButton} onClick={handleBid}>
           입찰
         </button>
