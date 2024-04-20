8fa4348e1e1d796439f3f0b72f78c6854125fa43
Sang Min Park <minisstar@naver.com>
2024-04-20 12:46:46 +0900

    feat: 최고가격 입찰시 현재가격 최고가격으로 갱신

--- src/components/PriceList.js
+++ src/components/PriceList.js
@@ -3,6 +3,7 @@ import styles from '../static/styles/css/PriceList.module.css';
 
 function PriceList({ items }) {
   const [sortedItems, setSortedItems] = useState([]); // 정렬된 목록 상태
+  const [currentPrice, setCurrentPrice] = useState('0원'); // 최고 가격 상태
 
   useEffect(() => {
     // 내림차순으로 정렬
@@ -13,12 +14,18 @@ function PriceList({ items }) {
     });
 
     setSortedItems(sorted); // 정렬된 목록 상태 설정
-  }, [items]); // items가 변경될 때마다 정렬
+
+    // 최고 가격을 설정합니다. 정렬된 배열의 첫 번째 요소를 사용합니다.
+    if (sorted.length > 0) {
+      setCurrentPrice(sorted[0].price);
+    }
+  }, [items]); // items가 변경될 때마다 정렬 및 최고 가격 업데이트
 
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <span className={styles.currentPriceTitle}>현재 가격</span>
+        <span className={styles.currentPrice}>{currentPrice}</span>
       </div>
       {/* 6개까지 표시하고, 그 이상은 스크롤 */}
       <div className={styles.itemList}>
