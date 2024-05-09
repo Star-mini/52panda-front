da34dcdbee4fa5ef094a80a5a6ffb00821e1178d
Sang Min Park <minisstar@naver.com>
2024-05-09 15:00:26 +0900

    feat: 질문등록 액시노스 연결

--- src/components/commons/etc/Add.js
+++ src/components/commons/etc/Add.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios'; // Axios 라이브러리를 임포트합니다.
 import styles from '../../../static/styles/css/Add.module.css'; // CSS 모듈을 임포트합니다.
 
 function Add({ onSubmit, onCancel }) {
@@ -9,8 +10,22 @@ function Add({ onSubmit, onCancel }) {
     };
 
     const handleCompleteClick = () => {
-        onSubmit(input);
-        setInput(""); // 입력 필드 초기화
+        // 환경 변수에서 API URL을 가져옵니다.
+        const apiUrl = `${process.env.REACT_APP_API_URL}/v1/auth/auction/3/qna/`;
+        
+        // Axios를 사용하여 POST 요청을 보냅니다.
+        axios.post(apiUrl, {
+            questionUserId: 1, // 여기에 실제 사용자 ID를 사용.
+            questionContents: input // 여기에 사용자가 입력한 질문 내용을 사용.
+        })
+        .then(response => {
+            console.log("데이터 전송 성공:", response.data);
+            onSubmit(input);
+            setInput(""); // 입력 필드 초기화
+        })
+        .catch(error => {
+            console.error("데이터 전송 실패:", error);
+        });
     };
 
     const handleCancelClick = () => {
@@ -28,6 +43,6 @@ function Add({ onSubmit, onCancel }) {
         </div>
       </div>
     );
-  }
+}
 
 export default Add;
