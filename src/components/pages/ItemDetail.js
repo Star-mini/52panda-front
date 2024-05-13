3df1d54f55d0bf10acdd66c57ebfdd8297d63312
Sang Min Park <minisstar@naver.com>
2024-05-13 19:54:25 +0900

    feat:물건상세 페이지 아이디에따라 보이는 화면 다르게 출력

--- src/components/pages/ItemDetail.js
+++ src/components/pages/ItemDetail.js
@@ -1,23 +1,33 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import { useLocation } from 'react-router-dom';
 import ProductDetail from "../commons/etc/ProductDetail";
 import AboutMore from "../commons/etc/AboutMore";
 
 function ItemDetail() {
   const [productData, setProductData] = useState(null);
+  const location = useLocation();
 
   useEffect(() => {
+    // URL의 쿼리 파라미터에서 itemId를 추출합니다.
+    const searchParams = new URLSearchParams(location.search);
+    const itemId = searchParams.get('itemId');
+
     const fetchData = async () => {
       try {
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/v1/auth/auction/1`);
-        setProductData(response.data.data);
+        if (itemId) {
+          const response = await axios.get(`${process.env.REACT_APP_API_URL}/v1/auth/auction/${itemId}`);
+          setProductData(response.data.data);
+        } else {
+          console.error('itemId가 URL에 없습니다.');
+        }
       } catch (error) {
         console.error('API 호출 오류:', error);
       }
     };
 
     fetchData();
-  }, []);
+  }, [location.search]);
 
   if (!productData) {
     return <div>Loading...</div>;
