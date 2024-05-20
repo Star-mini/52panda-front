05e796df5ac111d5c7e36342e9b31a6bd3882b69
Sang Min Park <minisstar@naver.com>
2024-05-21 08:41:00 +0900

    feat:추천 컴포넌트 추가

--- src/components/commons/etc/Recommend.js
+++ src/components/commons/etc/Recommend.js
@@ -1,11 +1,47 @@
 import React from "react";
+import styles from "../../../static/styles/css/Recommend.module.css";
+import RecommendItem from "./RecommendItem";
+import mac from '../../../static/styles/images/mac.png';
 
+const Recommend = () => {
+  const items = [
+    {
+      imageUrl: mac,
+      title: "13년쓴 맥북팔아요.",
+      price: "79900",
+    },
+    {
+      imageUrl: mac,
+      title: "13년쓴 맥북팔아요.",
+      price: "79900",
+    },
+    {
+      imageUrl: mac,
+      title: "13년쓴 맥북팔아요.",
+      price: "79900",
+    },
+    {
+      imageUrl: mac,
+      title: "13년쓴 맥북팔아요.",
+      price: "79900",
+    },
+  ];
 
-function Recommend() {
   return (
-    <div>
+    <div className={styles.container}>
+      <h2>추천 상품</h2>
+      <div className={styles.recommend__list}>
+        {items.map((item, index) => (
+          <RecommendItem
+            key={index}
+            imageUrl={item.imageUrl}
+            title={item.title}
+            price={item.price}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default Recommend;
