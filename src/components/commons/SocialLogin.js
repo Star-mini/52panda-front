ce8a9018b2567643e6fdb2e61391987fae654610
kjh <akkaa1119@naver.com>
2024-05-21 13:21:31 +0900

    feat : 소셜 로그인 링크 추가

--- src/components/commons/SocialLogin.js
+++ src/components/commons/SocialLogin.js
@@ -5,8 +5,8 @@ import Kakao from '../../static/styles/images/login//kakao_icon.png';
 import styles from "../../static/styles/css/SocialLogin.module.css"; // 모듈 CSS 임포트
 
 function SocialLogin() {
-  const handleSocialLogin = (provider) => {
-    console.log(`로그인 with ${provider}`);
+  const handleSocialLogin =  (url) => {
+    window.location.href = url;
   };
 
   return (
@@ -14,9 +14,9 @@ function SocialLogin() {
       <div className={styles.greenBox}>
         <h2 className={styles.welcomeMessage}>52판다에 오신 것을 환영합니다.<br />로그인을 하고 오늘은 이걸로 팔아보세요!</h2>
         <div className={styles.socialLoginButtons}>
-          <img src={Google} alt="Google" className={styles.socialButton} onClick={() => handleSocialLogin('Google')} />
-          <img src={Naver} alt="Naver" className={styles.socialButton} onClick={() => handleSocialLogin('Naver')} />
-          <img src={Kakao} alt="Kakao" className={styles.socialButton} onClick={() => handleSocialLogin('Kakao')} />
+          <img src={Google} alt="Google" className={styles.socialButton} onClick={() => handleSocialLogin(process.env.REACT_APP_GOOGLE_OAUTH_URL)} />
+          <img src={Naver} alt="Naver" className={styles.socialButton} onClick={() => handleSocialLogin(process.env.REACT_APP_NAVER_OAUTH_URL)} />
+          <img src={Kakao} alt="Kakao" className={styles.socialButton} onClick={() => handleSocialLogin(process.env.REACT_APP_KAKAO_OAUTH_URL)} />
         </div>
       </div>
     </div>
