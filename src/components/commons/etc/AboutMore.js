778b02ec0eb2824214641feb99bbcffa914c2c38
Sang Min Park <minisstar@naver.com>
2024-05-24 12:54:17 +0900

    feat:AboutMore에서 유저 닉네임 마스킹 기능 추가

--- src/components/commons/etc/AboutMore.js
+++ src/components/commons/etc/AboutMore.js
@@ -12,6 +12,18 @@ function AboutMore({ productData }) {
     qnaRef.current.scrollIntoView({ behavior: 'smooth' });
   };
 
+  // 한글인지 영어인지 감지하고 특정 문자를 '*'로 바꾸는 함수
+  const maskUsername = (username) => {
+    if (!username) return "Loading...";
+
+    const isKorean = /[가-힣]/.test(username[0]);
+    if (isKorean) {
+      return username.length > 1 ? username[0] + '*' + username.slice(2) : username;
+    } else {
+      return '*' + username.slice(1);
+    }
+  };
+
   return (
     <div className={styles.aboutMore}>
       <div className={styles.tabs}>
@@ -27,7 +39,7 @@ function AboutMore({ productData }) {
             className={styles.profileLogo}
           />
           <div className={styles.userDetails}>
-            <span className={styles.username}>{productData?.userNickname || "Loading..."}</span>
+            <span className={styles.username}>{maskUsername(productData?.userNickname)}</span>
             <span className={styles.registrationDate}>
               등록일자: {productData ? new Date(productData.itemCreateTime).toLocaleString() : "Loading..."}
             </span>
@@ -40,7 +52,7 @@ function AboutMore({ productData }) {
 
         {/* 여기에 ref를 추가합니다. */}
         <div ref={qnaRef}>
-          {/* QnA 컴포넌트삽입 */}
+          {/* QnA 컴포넌트 삽입 */}
           <QnA productData={productData} />
         </div>
       </div>
