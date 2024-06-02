714516c729ff72ac3c8c3b3de0ca8dcd17a91366
Sang Min Park <minisstar@naver.com>
2024-06-03 05:01:56 +0900

    feat:물건추천에서 클릭시 그 물건으로 렌더링

--- src/components/commons/etc/RecommendItem.js
+++ src/components/commons/etc/RecommendItem.js
@@ -2,9 +2,14 @@ import React from 'react';
 import styles from '../../../static/styles/css/RecommendItem.module.css';
 
 // RecommendItem 컴포넌트
-const RecommendItem = ({ imageUrl, title, price }) => {
+const RecommendItem = ({ itemId, imageUrl, title, price }) => {
+
+  const handleClick = (e) => {
+    window.location.href = `/detail?itemId=${itemId}`; // 클릭 시 페이지 강제 이동
+  };
+
   return (
-    <div className={styles.recommendItem}>
+    <div className={styles.recommendItem} onClick={handleClick}>
       <img src={imageUrl} alt={title} className={styles.recommendItem__image} />
       <div className={styles.recommendItem__details}>
         <p className={styles.recommendItem__title}>{title}</p>
