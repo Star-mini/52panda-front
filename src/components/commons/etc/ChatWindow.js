840991d69e005aa7f7258271d413cd996f9df07f
kjh <akkaa1119@naver.com>
2024-06-03 09:58:23 +0900

    fix : 채팅버그 수정

--- src/components/commons/etc/ChatWindow.js
+++ src/components/commons/etc/ChatWindow.js
@@ -141,7 +141,7 @@ function ChatWindow({ roomId, roomTitle, onBackButtonClick }) {
         {chatMessages.map((message, index) => (
           <div 
             key={index} 
-            className={`${styles.chatBubble} ${message.chatUser === localStorage.getItem("id") ? styles.right : styles.left}`}
+            className={`${styles.chatBubble} ${message.chatUser === parseInt(localStorage.getItem("id")) ? styles.right : styles.left}`}
             dangerouslySetInnerHTML={{ __html: message.content }} // HTML을 안전하게 렌더링
           />
         ))}
